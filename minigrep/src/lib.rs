e124231db1d90168883b692eb4f4902d16d2798d
Dylan Bartlett <2047912+bartlettD@users.noreply.github.com>
2019-09-24 13:40:42 +0100

    Moved core fuctions to lib.rs

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,27 @@
+use std::error::Error;
+use std::fs;
+
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+	let contents = fs::read_to_string(config.filename)?;
+
+	println!("With text:\n{}", contents);
+
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3{
+            return Err("Not enough arguments. Arguments: query, filename");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config {query, filename})
+    }
+}
